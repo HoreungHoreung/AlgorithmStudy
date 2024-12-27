3c3e51bf0a9fec1be22c9765ad6792ede5d392c2
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-27 21:25:38 +0900

    Title: 1시간 뒤 시간 출력, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241227/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 241227/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s= sc.next();
         String[] strArr = s.split(":");
-        int a = int(strArr[0]);
-        int b = int(strArr[1]);
+        int a = Integer.parseInt(strArr[0]);
+        int b = Integer.parseInt(strArr[1]);
         System.out.println(strArr[0] + 1 + ":" + strArr[1]);
     }
 }
