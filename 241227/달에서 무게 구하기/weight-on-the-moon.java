64291cefdefdb61e2be6f8f2ab8fc6830552fb76
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-27 18:09:18 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241227/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int chew = 13;
+        float ratio = 0.165;
+        System.out.printf("%d * %.6f = %.6f", chew, ratio, chew*ratio);
+    }
+}
