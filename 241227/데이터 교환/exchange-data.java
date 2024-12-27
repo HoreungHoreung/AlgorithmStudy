3efcaba4d26ef5fe56255509c26c1b9973a85e91
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-27 20:37:33 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241227/데이터 교환/exchange-data.java
+++ 241227/데이터 교환/exchange-data.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a = 5, b = 6, c = 7, temp1, temp2;
-        temp1 = b;
-        temp2 = c;
-        b = a;
-        c = b;
+        int a = 5, b = 6, c = 7, temp;
+        temp = a;
         a = c;
+        c = b;
+        b = temp
+
         System.out.printf("%d\n%d\n%d", a, b, c);
 
     }
