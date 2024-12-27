f91622c3b98fd1ce188a1bba3dc8e766fe7bfda0
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-27 19:49:12 +0900

    Title: 소수점 반올림하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241227/소수점 반올림하기/rounding-decimal-points.java
+++ 241227/소수점 반올림하기/rounding-decimal-points.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         double a = 25.352;
-        printf("%f.1f", a);
+        printf("%.1f", a);
     }
 }
