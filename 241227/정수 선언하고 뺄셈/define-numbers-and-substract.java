a6203f330644f8a8ebfb0fbd59206cb62b1b7ed4
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-27 17:57:22 +0900

    Title: 정수 선언하고 뺄셈, Time: 80ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241227/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 241227/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a, b = 97, 13;
-        int c = a - b;
-        System.out.println(a + "-" + b + "=" + c);
+        int a = 97, b = 13;
+        System.out.println(a + "-" + b + "=" + (a - b));
     }
 }
