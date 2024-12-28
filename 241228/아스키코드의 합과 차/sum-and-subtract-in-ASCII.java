27766dd0e3417d98543a3164f3bf55a2042b73cc
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-28 11:59:23 +0900

    Title: 아스키코드의 합과 차, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241228/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        
+        char first = sc.next().charAt(0);
+        char second = sc.next().charAt(0);
+        int a = (int) first;
+        int b = (int) second;
+
+        System.out.print(a + b + " " + a - b);
+    }
+}
