6cdcd4fba260491b367a9a1d48268271dca3fa09
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2024-12-29 11:50:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 476 ms, Memory: 106200 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,7 +1,9 @@
+import java.nio.Buffer;
 import java.util.Scanner;
+import java.io.*;
 public class Main {
-	public static void main(String[] args) {
-		int min = 1000000;
+	public static void main(String[] args) throws IOException{
+		/*int min = 1000000;
 		int max = -1000000;
 
 		Scanner sc = new Scanner(System.in);
@@ -16,6 +18,21 @@ public class Main {
 				max = num;
 			}
 		}
+		System.out.println(min + " " + max);*/
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] numbers = br.readLine().split(" ");
+
+		int max = Integer.parseInt(numbers[0]);
+		int min = Integer.parseInt(numbers[0]);
+
+		for (int i = 1; i < N; i++) {
+			max = Math.max(max, Integer.parseInt(numbers[i]));
+			min = Math.min(min, Integer.parseInt(numbers[i]));
+		}
+
 		System.out.println(min + " " + max);
+		br.close();
+
 	}
 }
