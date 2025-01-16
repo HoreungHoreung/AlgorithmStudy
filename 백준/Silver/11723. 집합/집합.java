fc5509f0432050abda1a742a005a09198f160c6b
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-01-16 11:06:34 +0900

    [Silver V] Title: 집합, Time: 1008 ms, Memory: 298364 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,37 +12,72 @@ public class Main {
 
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] set = new int[21][1];
-        //set[0][0] = 0;
+        Set<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             String command = st.nextToken();
-            if(command.equals("add")) {
-                int num = Integer.parseInt(st.nextToken());
-                if(set[num][0] == 0) set[num][0] = 1;
+            int num = 0;
+            if(st.hasMoreTokens()) {
+                num = Integer.parseInt(st.nextToken());
             }
-            else if(command.equals("remove")) {
-                int num = Integer.parseInt(st.nextToken());
-                if(set[num][0] == 1) set[num][0] = 0;
-            }
-            else if(command.equals("check")) {
-                int num = Integer.parseInt(st.nextToken());
-                bw.write(set[num][0] + "\n");
-            }
-            else if(command.equals("toggle")) {
-                int num = Integer.parseInt(st.nextToken());
-                if(set[num][0] == 1) set[num][0] = 0;
-                else set[num][0] = 1;
-            }
-            else if(command.equals("all")) {
-                for(int j = 0; j < 21; j++) set[j][0] = 1;
-            }
-            else {
-                set = new int[21][1];
+
+            switch(command) {
+                case "add" :
+                    set.add(num);
+                    break;
+                case "remove" :
+                    set.remove(num);
+                    break;
+                case "check" :
+                    if(set.contains(num)) bw.write(1 + "\n");
+                    else bw.write(0 + "\n");
+                    break;
+                case "toggle" :
+                    if(set.contains(num)) set.remove(num);
+                    else set.add(num);
+                    break;
+                case "all" :
+                    for(int j = 1; j <= 20; j++) {
+                        set.add(j);
+                    } break;
+                case "empty" :
+                    set.clear();
+                    break;
             }
         }
 
+
+//        int[][] set = new int[21][1];
+//        //set[0][0] = 0;
+//
+//        for (int i = 0; i < M; i++) {
+//            st = new StringTokenizer(br.readLine(), " ");
+//            String command = st.nextToken();
+//            if(command.equals("add")) {
+//                int num = Integer.parseInt(st.nextToken());
+//                if(set[num][0] == 0) set[num][0] = 1;
+//            }
+//            else if(command.equals("remove")) {
+//                int num = Integer.parseInt(st.nextToken());
+//                if(set[num][0] == 1) set[num][0] = 0;
+//            }
+//            else if(command.equals("check")) {
+//                int num = Integer.parseInt(st.nextToken());
+//                bw.write(set[num][0] + "\n");
+//            }
+//            else if(command.equals("toggle")) {
+//                int num = Integer.parseInt(st.nextToken());
+//                if(set[num][0] == 1) set[num][0] = 0;
+//                else set[num][0] = 1;
+//            }
+//            else if(command.equals("all")) {
+//                for(int j = 0; j < 21; j++) set[j][0] = 1;
+//            }
+//            else {
+//                set = new int[21][1];
+//            }
+//        }
         br.close();
         bw.flush();
         bw.close();
