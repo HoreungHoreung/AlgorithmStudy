5403b01b264e0ab794a6eb2185ac321129df3c79
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-01-21 20:37:47 +0900

    Title: 안전 지대, Time: 83ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250121/안전 지대/comfort-zone.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int N, M;
+    public static int cnt = 0;
+    public static int[][] grid;
+    public static int[][] visited;
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < M;
+    }
+
+    public static boolean canGo(int x, int y, int level) {
+        if(!inRange(x, y)) return false;
+        if(visited[x][y] == 1 || grid[x][y] <= level) return false;
+        return true;
+    }
+
+    public static void DFS(int x, int y, int level) {
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+
+        visited[x][y] = 1;
+
+        for(int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if(canGo(newX, newY, level)) DFS(newX, newY, level);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int maxSafe = 0;
+        int safeLevel = 0;
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        grid = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int level = 1; level <= 100; level++) {
+            visited = new int[N][M];
+            int safeZones = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(grid[i][j] > level && visited[i][j] == 0) {
+                        DFS(i, j, level);
+                        safeZones++;
+                    }
+                }
+            }
+
+            if (safeZones > maxSafe) {
+                safeLevel = level;
+                maxSafe = safeZones;
+            }
+        }
+
+        sb.append(safeLevel).append(" ").append(maxSafe);
+
+        bw.write(sb.toString());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
