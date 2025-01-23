0c26ab03fbc2f184b67efb8908254cfa942fa1fa
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-01-23 23:00:28 +0900

    Title: 국영수 순이지, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250123/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Student {
+    String name;
+    int kor;
+    int eng;
+    int math;
+    public Student(String name, int kor, int eng, int math) {
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Student[] students = new Student[n];
+
+        for(int i = 0; i < n; i++) {
+            String name = sc.next();
+            int kor = sc.nextInt();
+            int eng = sc.nextInt();
+            int math = sc.nextInt();
+
+            students[i] = new Student(name, kor, eng, math);
+        }
+
+        Arrays.sort(students, new Comparator<Student>() {
+            
+            public int compare(Student s1, Student s2) {
+                if(s1.kor != s2.kor) return s2.kor - s1.kor;
+                if(s1.eng != s2.eng) return s2.eng - s1.eng;
+                else return s2.math - s1.math;
+            }
+        });
+
+        for(Student student : students) {
+            System.out.println(student.name + " " + student.kor + " " + student.eng + " " + student.math);
+        }
+
+    }
+}
