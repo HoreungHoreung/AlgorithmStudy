f937a746b598bb1ecdf48345570745fcd98b815e
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-01-25 22:07:10 +0900

    Title: 행복한 수열의 개수, Time: 242ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 250125/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+    
+    public static int[][] grid1;
+    public static int[][] grid2;
+    
+    public static boolean isHappy(int[] currNums, int m) {
+        int sameNum = currNums[0];
+        int sameCnt = 1;
+        for(int i = 1; i < currNums.length; i++){
+            if(sameNum == currNums[i]) {
+                sameCnt++;
+            }
+            else{
+                sameNum = currNums[i];
+                sameCnt = 1;
+            }
+        }
+        if (sameCnt >= m) return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        grid1 = new int[n][n];
+        grid2 = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                grid1[i][j] = grid2[j][i] = sc.nextInt();
+            }
+        }
+
+        int happys = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(isHappy(grid1[i], m)) happys++;
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(isHappy(grid2[j], m)) happys++;
+        }
+
+        System.out.println(happys);
+    }
+}
