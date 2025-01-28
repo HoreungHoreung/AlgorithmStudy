6fe3a8a0d450743464c386b8af9bef4ae0b354a3
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-01-28 14:37:35 +0900

    Title: 아름다운 수, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250128/아름다운 수/beautiful-number.java
+++ 250128/아름다운 수/beautiful-number.java
@@ -1,33 +1,39 @@
 import java.util.*;
+
 public class Main {
-    
     private static ArrayList<Integer> array = new ArrayList<>();
     private static int n;
     private static int cnt = 0;
-    
-    public static void choose(int currNum) {
-        if(array.size() == n) {
+
+    public static void choose(int remaining) {
+        // 배열 크기가 n에 도달하면 경우의 수를 증가
+        if (remaining == 0) {
             cnt++;
             return;
-        } else if(currNum > n) return;
-        
-        for(int i = 1; i <= n; i++) {
-            for(int j = 0; j < i; j++) {
+        }
+
+        // 가능한 숫자를 탐색 (1부터 n까지)
+        for (int i = 1; i <= remaining; i++) {
+            // 숫자 i를 추가
+            for (int j = 0; j < i; j++) {
                 array.add(i);
             }
-            choose(currNum + 1);
-            for(int j = 0; j < i; j++) {
+
+            // 백트래킹: 남은 공간에서 i만큼 차감
+            choose(remaining - i);
+
+            // 숫자 i를 제거 (백트래킹)
+            for (int j = 0; j < i; j++) {
                 array.remove(array.size() - 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
 
-        choose(1);
+        choose(n); // 총 n개의 숫자를 채우는 경우 탐색
         System.out.println(cnt);
     }
 }
