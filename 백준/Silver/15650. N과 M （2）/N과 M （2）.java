733443bf38555ee031a3525620fc874d80a446b7
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-02-02 12:25:46 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static boolean[] visited;
+    private static int[] arr;
+
+    private static void permutation(int cnt) {
+        if(cnt == M) {
+            for(int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i <= N;i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            arr[cnt] = i;
+            permutation(cnt + 1);
+            visited[i] = false;
+        }
+    }
+
+    private static void combination(int start, int cnt) {
+        if(cnt == M) {
+            for(int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = start; i <= N; i++) {
+            arr[cnt] = i;
+            combination(i + 1, cnt + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visited = new boolean[N + 1];
+        arr = new int[M];
+
+        sc.close();
+
+//        permutation(0);
+        combination(1, 0);
+    }
+
+}
+
