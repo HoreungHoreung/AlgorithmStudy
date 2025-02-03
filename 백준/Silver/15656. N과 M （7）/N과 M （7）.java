386ca65b7db59ef1eb62b8197b4751e9c6a101a4
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-02-03 11:23:23 +0900

    [Silver III] Title: N과 M (7), Time: 820 ms, Memory: 407880 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] arr;
+    private static int[] nums;
+    private static StringBuilder sb = new StringBuilder();
+
+    private static void repeatedPermutation(int cnt) {
+        if(cnt == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            arr[cnt] = nums[i];
+            repeatedPermutation(cnt + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        nums = new int[N];
+        for(int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        repeatedPermutation(0);
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
