de5d3f61240b95ae1bf4a397b518087da70290d3
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-02-17 14:53:03 +0900

    [Silver III] Title: N과 M (8), Time: 96 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,32 +1,17 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
     private static int N, M;
     private static int[] arr;
+    private static boolean[] visited = new boolean[10001];
     private static int[] nums;
     private static StringBuilder sb = new StringBuilder();
-
-    private static void repeatedPermutation(int cnt) {
-        if(cnt == M) {
-            for(int i = 0; i < M; i++) {
-                sb.append(arr[i] + " ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for(int i = 0; i < N; i++) {
-            arr[cnt] = nums[i];
-            repeatedPermutation(cnt + 1);
-        }
-    }
 
     private static void repeatedCombination(int start, int cnt) {
         if(cnt == M) {
             for(int i = 0; i < M; i++) {
-                sb.append(arr[i] + " ");
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
@@ -34,7 +19,7 @@ public class Main {
 
         for(int i = start; i < N; i++) {
             arr[cnt] = nums[i];
-            repeatedCombination(i ,cnt + 1);
+            repeatedCombination(i, cnt + 1);
         }
     }
 
@@ -46,20 +31,19 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[M];
+        nums = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        nums = new int[N];
         for(int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(nums);
-//        repeatedPermutation(0);
         repeatedCombination(0, 0);
-        br.close();
+
         bw.write(sb.toString());
+        br.close();
         bw.flush();
         bw.close();
-
     }
 }
