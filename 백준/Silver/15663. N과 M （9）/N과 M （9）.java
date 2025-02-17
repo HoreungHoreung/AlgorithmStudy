0d50e414b1cbaa1e9d852de7af9546c618a1b7ac
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-02-17 15:13:57 +0900

    [Silver II] Title: N과 M (9), Time: 744 ms, Memory: 126092 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -4,43 +4,44 @@ import java.io.*;
 public class Main {
     private static int N, M;
     private static int[] arr;
+    private static int[] visited = new int[10001];
     private static int[] nums;
-    private static int[]visited = new int[10001];
     private static StringBuilder sb = new StringBuilder();
-    private static HashSet<String> output = new HashSet<>();
+    private static HashSet<String> hashSet = new HashSet<>();
 
     private static void permutation(int cnt) {
         if(cnt == M) {
-            String key = Arrays.toString(arr);
-            if(output.contains(key)) return;
-            output.add(key);
+            StringBuilder temp = new StringBuilder();
             for(int i = 0; i < M; i++) {
-                sb.append(arr[i] + " ");
+                temp.append(arr[i]).append(" ");
             }
-            sb.append("\n");
-            return;
-        }
-
-        for(int num : nums) {
-            if(visited[num] != 0) {
-                arr[cnt] = num;
-                visited[num]--;
-                permutation(cnt + 1);
-                visited[num]++;
+            if(hashSet.contains(temp.toString())) {
+                return;
+            } else {
+                for(int i = 0; i < M; i++) {
+                    sb.append(arr[i]).append(" ");
+                }
+                sb.append("\n");
+                hashSet.add(temp.toString());
+                return;
             }
         }
-
+        for(int i = 0; i < N; i++) {
+            if(visited[nums[i]] == 0) continue;
+            visited[nums[i]]--;
+            arr[cnt] = nums[i];
+            permutation(cnt + 1);
+            visited[nums[i]]++;
+        }
     }
 
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         arr = new int[M];
         nums = new int[N];
 
@@ -52,12 +53,11 @@ public class Main {
         }
 
         Arrays.sort(nums);
-
         permutation(0);
-        br.close();
+
         bw.write(sb.toString());
+        br.close();
         bw.flush();
         bw.close();
     }
-
 }
