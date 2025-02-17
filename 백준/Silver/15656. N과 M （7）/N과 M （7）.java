fd7a10b026d46f9e6e2aed9ff9da742d50c5fbfb
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-02-17 14:36:12 +0900

    [Silver III] Title: N과 M (7), Time: 552 ms, Memory: 220548 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,29 +1,30 @@
-
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
+    private static final int MAX_N = 10000;
     private static int N, M;
     private static int[] arr;
+    private static boolean[] visited = new boolean[MAX_N + 1];
     private static int[] nums;
     private static StringBuilder sb = new StringBuilder();
 
     private static void repeatedPermutation(int cnt) {
         if(cnt == M) {
             for(int i = 0; i < M; i++) {
-                sb.append(arr[i] + " ");
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
-
-        for(int i = 0; i < N; i++) {
-            arr[cnt] = nums[i];
+        for(int num: nums) {
+            arr[cnt] = num;
             repeatedPermutation(cnt + 1);
         }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,19 +32,20 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[M];
+        nums = new int[N];
+
 
         st = new StringTokenizer(br.readLine());
-        nums = new int[N];
         for(int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(nums);
         repeatedPermutation(0);
-        br.close();
+
         bw.write(sb.toString());
+        br.close();
         bw.flush();
         bw.close();
-
     }
 }
