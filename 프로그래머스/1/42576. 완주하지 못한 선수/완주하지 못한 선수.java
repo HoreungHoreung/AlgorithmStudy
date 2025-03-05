02c1a3a4e23de51f463d8acc7c8b493ed5b18b21
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-03-05 19:59:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.36 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,18 +2,16 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> participantMap = new HashMap<>();
-        
-        for (String p : participant) {
+        for(String p : participant){
             participantMap.put(p, participantMap.getOrDefault(p, 0) + 1);
         }
-        
-        for (String c : completion) {
+        for(String c : completion) {
             participantMap.put(c, participantMap.get(c) - 1);
         }
         
-        for (Map.Entry<String, Integer> entry : participantMap.entrySet()) {
+        for(Map.Entry<String, Integer> entry : participantMap.entrySet()){
             if(entry.getValue() > 0) return entry.getKey();
-        }
+        } 
         return "";
     }
 }
