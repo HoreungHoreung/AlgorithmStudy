2c3dd089630f17fa61c7ff957c99ecacb803baf9
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-03-11 11:50:15 +0900

    [level 2] Title: 타겟 넘버, Time: 207.17 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,14 +1,23 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers, int target) {
-        int answer = dfs(numbers, target, 0, 0);
-        return answer;
-    }
-    private static int dfs(int[] numbers, int target, int index, int sum) {
-        if(index == numbers.length) {
-            return sum == target ? 1 : 0;
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        
+        queue.offer(new int[]{0, 0});
+        
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int sum = current[0];
+            int index = current[1];
+            
+            if(index == numbers.length) {
+                if(sum == target) answer++;
+            } else {
+                queue.offer(new int[] {sum + numbers[index], index + 1});
+                queue.offer(new int[] {sum - numbers[index], index + 1});
+            }
         }
-        
-        return dfs(numbers, target, index + 1, sum + numbers[index]) + dfs(numbers, target, index + 1, sum - numbers[index]);
-        
+        return answer;
     }
 }
