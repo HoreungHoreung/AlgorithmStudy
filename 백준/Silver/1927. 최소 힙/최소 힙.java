192c30bd908f47587781ee888b1f690b64ee6265
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-03-30 22:26:13 +0900

    [Silver II] Title: 최소 힙, Time: 260 ms, Memory: 25460 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,21 +1,22 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
 
-
-        int N = sc.nextInt();
         for(int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            if(x == 0) {
-                if (minHeap.isEmpty()) System.out.println(0);
-                else {
-                    System.out.println(minHeap.poll());
-                }
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                sb.append(minHeap.isEmpty() ? 0 : minHeap.poll()).append("\n");
             } else {
                 minHeap.offer(x);
             }
         }
+        System.out.print(sb);
     }
 }
