5576574a98304033d328698f5c1d12775a73e83e
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-04-04 09:46:29 +0900

    [Silver IV] Title: 수 찾기, Time: 500 ms, Memory: 58744 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,51 +1,54 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 public class Main {
-    private static boolean binary_search(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
+    public static boolean search(int target, int[] A) {
+        int start = 0;
+        int end = A.length - 1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) return true;
-            else if (arr[mid] < target) {
-                left = mid + 1;
+        while(start <= end) {
+            //가운데 인덱스 선정
+            int mid = (start + end) / 2;
+            //가운데 인덱스가 타겟 넘버면 반환
+            if(A[mid] == target) return true;
+            else if(target <  A[mid]) {
+                end = mid - 1;
             } else {
-                right = mid - 1;
+                start = mid + 1;
             }
         }
+
         return false;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
+        //주어진 정수들 입력
+        int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
+        
+        //A배열 오름차순 정렬
         Arrays.sort(A);
 
-        int M = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-        int[] B = new int[M];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < M; i++) {
-            B[i] = Integer.parseInt(st.nextToken());
+        //확인해야할 정수들 입력
+        int M = Integer.parseInt(br.readLine());
+        int[] toCheck = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M ; i++) {
+            toCheck[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < M; i++) {
-            if(binary_search(A,B[i])) {
-                bw.write("1\n");
-            } else {
-                bw.write("0\n");
-            }
+        for(int num : toCheck) {
+            sb.append(search(num, A) ? "1\n" : "0\n");
         }
+
         br.close();
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
