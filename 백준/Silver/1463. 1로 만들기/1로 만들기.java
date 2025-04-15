5712cf1ee7f452e87c106328cb0bb4e815f21812
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-04-15 11:37:27 +0900

    [Silver III] Title: 1로 만들기, Time: 88 ms, Memory: 15576 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    private static int makeOne(int X) {
+        int[] dp = new int[X+1];
+        dp[1] = 0;
+        for(int i = 2; i <= X; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if(i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if(i % 3 == 0 ) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        return dp[X];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int X = Integer.parseInt(br.readLine());
+
+        br.close();
+        bw.write(Integer.toString(makeOne(X)));
+        bw.flush();
+        bw.close();
+    }
+}
