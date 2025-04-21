f46e9f56e390c037a0e6901eb9a8b9db5495aade
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-04-21 13:08:12 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 80 ms, Memory: 12112 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,34 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
+//시간복잡도 -> O(nlogn)
 public class Main {
+    private static int binarySearch(LinkedList<Integer> nums, int start, int end, int num) {
+        while(start < end) {
+            int mid = (start + end) / 2;
+            int midNum = nums.get(mid);
+            if(midNum == num) return mid;
+            if(midNum < num) start = mid + 1;
+            else end = mid;
+        }
+        return (start + end) / 2;
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //전체 숫자 갯수 입력
         int N = Integer.parseInt(br.readLine());
+
+        //숫자 배열 입력
+        int[] nums = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-
-        int[] numbers = new int[N];
         for(int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] length = new int[N];
+        LinkedList<Integer> dp = new LinkedList<>();
 
-        int maxCnt = 1;
-        for(int i = 0; i < N; i++) {
-            length[i] = 1;
-            for(int j = 0; j < i; j++) {
-                if(numbers[j] > numbers[i]) {
-                    length[i] = Math.max(length[i], length[j] + 1);
-                }
+        dp.add(nums[N - 1]);
+        for(int i = nums.length - 2; i >= 0; i--) {
+            int currNum = nums[i];
+            int lastNum = dp.getLast();
+            if(lastNum < currNum) dp.add(currNum);
+            else {
+                int newEnd = binarySearch(dp, 0, dp.size() - 1, currNum);
+                dp.set(newEnd, currNum); //set함수로 덮어쓰기
             }
         }
 
-        for(int num : length) {
-            maxCnt = Math.max(maxCnt, num);
-        }
-
-        System.out.println(maxCnt);
+        int result = dp.size();
+        br.close();
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
     }
 }
