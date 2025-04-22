36abbd733cc480e4b37363c335cfaec84918849b
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-04-22 15:30:53 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,44 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int[] arr;
-    private static boolean[] visited;
-    private static int N, M;
-    private static StringBuilder sb = new StringBuilder();
-
-    private static void combination(int start, int cnt) {
-        if(cnt == M) {
-            for(int i = 0; i < M; i++) {
-                sb.append(arr[i] + " ");
+    public static StringBuilder sb;
+    public static int[] answer;
+    private static void permutation(boolean[] visited, int cnt, int start) {
+        if(cnt == answer.length) {
+            for(int i = 0; i < answer.length; i++) {
+                sb.append(answer[i] + " ");
             }
             sb.append("\n");
             return;
         }
-        for(int i = start; i <= N; i++) {
-            if(visited[i]) continue;
-            visited[i] = true;
-            arr[cnt] = i;
-            combination(i + 1, cnt + 1);
-            visited[i] = false;
-        }
-    }
 
+        for(int i = start; i < visited.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                answer[cnt] = i;
+                permutation(visited, cnt + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
-        visited = new boolean[N + 1];
+        answer = new int[M];
+        boolean[] visited = new boolean[N + 1];
 
-        combination(1, 0);
+        permutation(visited, 0,  1);
+
+        br.close();
         bw.write(sb.toString());
         bw.flush();
-        br.close();
         bw.close();
     }
 }
