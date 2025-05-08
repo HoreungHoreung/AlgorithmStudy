a4aac569f6c62bc6ab95d13e73fafb1e52f7bd1d
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-05-08 15:49:04 +0900

    [level 2] Title: 올바른 괄호, Time: 13.90 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,30 +1,21 @@
 import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+        Deque<Character> deque = new ArrayDeque<>();
         
-        Deque<Character> deque = new LinkedList<>();
         for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '(') {
-                deque.addLast(c);
-            } else {
-                if(deque.isEmpty()) {
-                    answer = false;
-                    break;
-                }
-                char prev = deque.pollLast();
-                if (prev != '(') {
-                    answer = false;
-                    break;
-                }
+            char curr = s.charAt(i);
+            if(curr == '(') deque.offerLast(curr);
+            else if(curr == ')') {
+                if(deque.isEmpty()) return false;
+                deque.pollLast();
             }
         }
         
-        if(deque.size() != 0) {
-            return false;
-        }
-        
+        if(!deque.isEmpty()) answer = false; 
+
         return answer;
     }
 }
