afdfac48bab8bfa89a7984bb0785a5d4d4609fe3
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-08-23 14:40:43 +0900

    [Silver IV] Title: 2+1 세일, Time: 924 ms, Memory: 31808 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> diaries = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            diaries.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(diaries, (a, b) ->  b - a );
+
+        int totalPay = 0;
+        while(diaries.size() > 2) {
+            totalPay += diaries.remove(0);
+            totalPay += diaries.remove(0);
+            diaries.remove(0);
+        }
+
+        for(int i = 0; i < diaries.size(); i++) {
+            totalPay += diaries.get(i);
+        }
+
+        System.out.println(totalPay);
+    }
+}
