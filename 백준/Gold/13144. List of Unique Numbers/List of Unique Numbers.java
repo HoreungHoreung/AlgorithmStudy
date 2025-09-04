3dd00c06535adbc901053d40c11ff8ac8d83f9a3
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-09-04 17:14:54 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 192 ms, Memory: 26320 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,34 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        int[] nums = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int max = 0;
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, arr[i]);
+		}
 
-        boolean[] visited = new boolean[100000 + 1];
+		int right = 0;
+		int left = 0;
+		long cnt = 1;
 
-        long cnt = 0;
-        int r = 0;
-        for (int l = 0; l < N; l++) {
-            while (r < N && !visited[nums[r]]) {
-                visited[nums[r]] = true;
-                r++;
-            }
-            cnt += (r - l);
+		boolean[] visited = new boolean[max+1];
+		visited[arr[0]] = true;
 
-            visited[nums[l]] = false;
-        }
+		while(right < n) {
+			right++;
+			if(right >= n) break;
+			if(!visited[arr[right]]) {
+				visited[arr[right]] = true;
+				cnt += right - left + 1;
+			} else{
+				while(left <= right) {
+					if (arr[left] == arr[right]) {
+						left++;
+						break;
+					}
+					visited[arr[left]] = false;
+					left++;
+				}
+				cnt += right - left + 1;
+			}
+		}
 
-        System.out.println(cnt);
-    }
+		System.out.println(cnt);
+	}
 }
-
-/*
-
-1 2 3 2 2 1
- */
